c74ff66d5e1f28b585338c66f5696439c43544d8
Carl Moore <carlmoore256@gmail.com>
2024-05-24 22:42:34 -0700

    Initial Commit

---
+++ ARVirtualCam/ARVirtualCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARVirtualCamApp.swift
+//  ARVirtualCam
+//
+//  Created by Carl Moore on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ARVirtualCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
